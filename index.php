bcc0de877aed0224513e1c27d8c2b4cff6999862
Naoto Omiya <omiya-pan@users.noreply.github.com>
2018-01-16 22:00:17 +0900

    とりあえず完成

--- index.php
+++ index.php
@@ -6,38 +6,76 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <h1>LINE Backup Viewer</h1>
-    <form name="fileform" method="post" action="view.php" target="_blank" enctype="multipart/form-data">
-      <label>自分の名前：<input type="text" name="username" value="" placeholder="LINEのユーザー名"></label>
-      <label>バックアップファイル（テキスト形式）：<input type="file" name="file" accept=".txt"></label>
-      <input type="submit" value="送信">
-    </form>
-    <div id="line_container">
-      <header>
-        <h2>ABCDEFGあああああああああああああああああああああああのトーク履歴</h2>
-      </header>
-      <div id="messages">
-        <div class="date">2017/10/01</div>
-        <div class="message message_left">
-          <div class="name">おーみや</div>
-          <div class="content">こんばんは</div>
-          <div class="time">22:14</div>
+    <div id="top_container">
+      <h1>LINE Backup Viewer</h1>
+      <p>スマートフォン版LINEアプリで保存したテキスト形式のトーク履歴バックアップファイルを見やすく表示します。</p>
+      <form name="fileform" method="post" action="view.php" target="_blank" enctype="multipart/form-data">
+        <div class="form_row">
+          <label>バックアップファイル：</label>
+          <input type="file" name="file" accept=".txt">
         </div>
-        <div class="notice">2017/10/01<br>あいうえおがグループに招待しました。</div>
-        <div class="message message_right">
-          <div class="content">こんばんはこんばんはこんばんはこんばんはこんばんはこんばんはこんばんは</div>
-          <div class="time">22:21</div>
+        <div class="form_row">
+          <label>自分のユーザー名：</label>
+          <input type="text" name="username" value="" placeholder="">
         </div>
-        <div class="message message_left">
-          <div class="content">こんばんは</div>
-          <div class="time">22:32</div>
+        <div class="form_row" style="text-align: center;">
+          <input type="submit" value="送信">
         </div>
-        <div class="message message_left">
-          <div class="name">おーみや</div>
-          <div class="content">こんばんはんばんはこんばんはこんばんはこん</div>
-          <div class="time">22:34</div>
+      </form>
+      <h2>LINE Backup Viewerとは</h2>
+      <p>LINE Backup Viewerは、スマートフォン内に保存されているLINEのトーク履歴データを使って、トーク履歴の始まりから終わりまでを自由に検索したりさかのぼったりして見返すことができるツールです。</p>
+      <ul>
+        <li>スマートフォン版LINEにトーク履歴のデータが残っていれば、すべてのメッセージを表示することができます。</li>
+        <li>指定した日付にすぐジャンプすることができます。最新のメッセージからさかのぼる必要はありません。</li>
+        <li>日ごとのメッセージ件数や、個人トーク、グループトークにおける人ごとのメッセージ件数・割合がわかります。</li>
+      </ul>
+      <p>トーク履歴は以下のように表示されます。日付ジャンプ機能やメッセージ件数の割合表示は実際に使って確かめてみてください。</p>
+      <div id="line_container" style="width: 25rem; margin: 0 auto;">
+        <header>
+          <h2>パン屋さんのトーク履歴</h2>
+        </header>
+        <div id="messages">
+          <div class="date">2017/10/01(日)</div>
+          <div class="message message_left">
+            <div class="name">パン</div>
+            <div class="content">こんばんは</div>
+            <div class="time">22:14</div>
+          </div>
+          <div class="date">2017/10/02(月)</div>
+          <div class="message message_right">
+            <div class="content">まだ起きてる人いる？</div>
+            <div class="time">1:33</div>
+          </div>
+          <div class="message message_left">
+              <div class="name">パン</div>
+              <div class="content">起きてるよ</div>
+            <div class="time">1:35</div>
+          </div>
+          <div class="message message_left">
+            <div class="name">ライス</div>
+            <div class="content">こんばんは</div>
+            <div class="time">1:36</div>
+          </div>
         </div>
       </div>
+      <h2>使い方</h2>
+      <ol>
+        <li>お手持ちのスマートフォンでLINEアプリを起動し、トーク履歴を保存したいトークのトーク画面を開きます。</li>
+        <li>トーク設定画面を開き、「トーク履歴をバックアップ」から「テキスト形式でバックアップ」を選択します。</li>
+        <li>Googleドライブなど、パソコンからファイルを参照できる場所にバックアップファイルを保存します。</li>
+        <li>このページを開き、バックアップファイルを選択します。</li>
+        <li>トーク画面でメッセージを右側に表示するユーザーのユーザー名（基本的に自分のユーザー名）を入力します。</li>
+        <li>「送信」ボタンをクリックすると、トーク履歴が新しいウィンドウ（タブ）で表示されます。</li>
+        <li>右上の日付一覧のうちいずれかの日付をクリックすると、その日付のメッセージにジャンプします。</li>
+        <li>右下にはメッセージの送信者ごとの件数と割合が円グラフで表示されます。</li>
+      </ol>
+      <h2>注意事項</h2>
+      <ul>
+        <li>このツールはパソコン専用です。また、Internet ExplorerとEdgeには対応していません。動作確認環境はGoogle Chrome 63です。</li>
+        <li>バックアップファイルに含まれるメッセージ件数にかかわらずすべてのメッセージを一度に表示するため、メッセージ件数の多いトーク履歴を送信すると、表示に時間がかかったり、動作が重くなったりすることがあります。</li>
+        <li>スマートフォン版LINEアプリの見た目に似せたデザインになっていますが、送信者のアイコン画像が表示されなかったり、フォントが環境に依存したものになったりして忠実な再現にはなっていないため、故意に編集されたトーク履歴ファイルを使ってもLINEの画面を偽装することはできません。</li>
+        <li>選択したバックアップファイルは一時的にサーバーにアップロードされますが、メッセージを読み込む作業中に使われるもので、処理が終わると同時に自動的に削除されます。そのため、サーバー上にプライバシーに関わるデータが残ることはありません。ただし、送信ボタンをクリックした後、ページがすべて表示されるまでの処理中に画面を閉じた場合に関しては、確実なファイル削除が保証できないため、処理中に画面を閉じないようにしてください。</li>
+      </ul>
     </div>
   </body>
 </html>
--- view.php
+++ view.php
@@ -2,6 +2,13 @@
 ini_set("display_errors", On);
 error_reporting(E_ALL);
 
+function get_dayofweek($date) {
+  $dow = array("日", "月", "火", "水", "木", "金", "土");
+  $datetime = new DateTime($date);
+
+  return $dow[$datetime->format("w")];
+}
+
 $username = $_POST["username"];
 $has_error = false;
 $error_msg = "";
@@ -9,6 +16,9 @@ $filename = "";
 $content = "";
 $title = "";
 $messages = array();
+$dates = array();
+$message_num_date = array();
+$message_num_person = array();
 
 try {
   if (!isset($_FILES["file"])) {
@@ -43,13 +53,15 @@ try {
       switch (count($items)) {
         case 1:
           if (strpos($items[0], "[LINE]") !== false) {
-            $title = $items[0];
+            $title = str_replace("[LINE] ", "", $items[0]);
           }
-          else if (strpos($items[0], "保存日時")) {
+          else if (strpos($items[0], "保存日時") !== false) {
 
           }
           else {
             $date = explode("(", $items[0])[0];
+            $dates[] = $date;
+            $message_num_date[$date] = 0;
           }
         
           break;
@@ -58,18 +70,27 @@ try {
           $messages[] = array(
             "date" => $date,
             "time" => explode("\t", $line)[0],
-            "content" => explode("\t", $line)[1],
+            "content" => str_replace("\n", "<br>", htmlspecialchars(explode("\t", $line)[1])),
           );
       
           break;
 
         case 3:
+          $name = explode("\t", $line)[1];
           $messages[] = array(
             "date" => $date,
             "time" => explode("\t", $line)[0],
-            "name" => explode("\t", $line)[1],
-            "content" => explode("\t", $line)[2]
+            "name" => htmlspecialchars($name),
+            "content" => str_replace("\n", "<br>", htmlspecialchars(explode("\t", $line)[2]))
           );
+          $message_num_date[$date] += 1;
+
+          if (array_key_exists($name, $message_num_person)) {
+            $message_num_person[$name] += 1;
+          }
+          else {
+            $message_num_person[$name] = 1;
+          }
         
           break;
       }
@@ -87,9 +108,49 @@ catch (RuntimeException $e) {
     <meta charset="UTF-8">
     <title>LINE Backup Viewer</title>
     <link rel="stylesheet" href="style.css">
+<?php
+if ($has_error == false) {
+?>
+    <script src="https://www.gstatic.com/charts/loader.js"></script>
+    <script>
+      google.charts.load('current', {'packages': ['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', '名前');
+        data.addColumn('number', 'メッセージ数');
+        data.addRows([
+<?php
+  arsort($message_num_person);
+
+  foreach ($message_num_person as $person => $num) {
+?>
+        ['<?php echo $person; ?>', <?php echo $num; ?>],
+<?php
+  }
+?>
+      ]);
+
+        var formatter = new google.visualization.NumberFormat({
+          suffix: '件',
+          fractionDigits: 0
+        });
+        formatter.format(data, 1);
+
+        var options = {
+          'fontSize': 16
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart'));
+        chart.draw(data, options);
+      }
+    </script>
+<?php
+}
+?>
   </head>
   <body>
-    <h1>LINE Backup Viewer</h1>
 <?php
 if ($has_error == true) {
 ?>
@@ -98,44 +159,45 @@ if ($has_error == true) {
 }
 else {
 ?>
-    <div id="line_container">
-      <header>
-        <h2><?php echo $title; ?></h2>
-      </header>
-      <div id="messages">
+    <div id="container">
+      <div id="line_container">
+        <header>
+          <h2><?php echo htmlspecialchars($title); ?></h2>
+        </header>
+        <div id="messages">
 <?php
   $prev_date = "";
 
   foreach ($messages as $message) {
     if ($message["date"] != $prev_date) {
 ?>
-        <div class="date"><?php echo $message["date"]; ?></div>
+          <div class="date" id="<?php echo str_replace("/", "", $message["date"]); ?>"><?php echo $message["date"]; ?>(<?php echo get_dayofweek($message["date"]); ?>)</div>
 <?php
     }
 
     switch (count($message)) {
       case 3:
 ?>
-        <div class="notice"><?php echo $message["time"]; ?><br><?php echo $message["content"]; ?></div>
+          <div class="notice"><?php echo $message["time"]; ?><br><?php echo $message["content"]; ?></div>
 <?php
         break;
     
       case 4:
         if ($message["name"] == $username) {
 ?>
-        <div class="message message_right">
-          <div class="content"><?php echo $message["content"]; ?></div>
-          <div class="time"><?php echo $message["time"]; ?></div>
-        </div>
+          <div class="message message_right">
+            <div class="content"><?php echo $message["content"]; ?></div>
+            <div class="time"><?php echo $message["time"]; ?></div>
+          </div>
 <?php
         }
         else {
 ?>
-        <div class="message message_left">
-          <div class="name"><?php echo $message["name"]; ?></div>
-          <div class="content"><?php echo $message["content"]; ?></div>
-          <div class="time"><?php echo $message["time"]; ?></div>
-        </div>
+          <div class="message message_left">
+            <div class="name"><?php echo $message["name"]; ?></div>
+            <div class="content"><?php echo $message["content"]; ?></div>
+            <div class="time"><?php echo $message["time"]; ?></div>
+          </div>
 <?php            
         }
 
@@ -144,9 +206,26 @@ else {
 
     $prev_date = $message["date"];
   }
-}
 ?>
+        </div>
+      </div>
+      <div id="date_select">
+        <ul>
+<?php
+  foreach ($dates as $date) {
+?>
+          <li><a class="datelink" href="#<?php echo str_replace("/", "", $date); ?>"><div class="datelink_date"><?php echo $date; ?>(<?php echo get_dayofweek($date); ?>)</div><div class="datelink_num"><?php echo $message_num_date[$date]; ?>件</div></a></li>
+<?php
+  }
+?>
+        </ul>
+      </div>
+      <div id="person_chart">
+        <div id="chart" style="width: 100%; height: 100%;"></div>
       </div>
     </div>
+<?php
+}
+?>
   </body>
 </html>
